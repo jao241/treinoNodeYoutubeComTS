03b97b7c010b2121c17cea9e6d67b733ca578ba7
joao vitor <vorque45@gmail.com>
2021-07-12 17:05:17 -0300

    Curso concluido

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,24 @@
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 import path from 'path';
+import { errors } from 'celebrate';
 
 const app = express();
+
+app.use(cors());
+// app.use(cors({
+//     origin: ['dominio.com.br']
+// }));
+
+app.use(express.json());
 
 app.use(routes);
 
 //Tem que pegar as imagens no github
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(3333, ()=>{
     console.log('Server started on port 3333!')
